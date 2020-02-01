659e0254bac837540d54a37c4231956f73d061f9
Hari Mohan <harikannan8055@gmail.com>
2020-02-01 15:00:19 +0530

    Revert "initial"
    This reverts commit 584f705b5d73f89acb7a3cbe75130834d0217c46.

--- goodspool/goodspool/settings.py
+++ goodspool/goodspool/settings.py
@@ -75,13 +75,9 @@ WSGI_APPLICATION = 'goodspool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'goodspool_test',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
